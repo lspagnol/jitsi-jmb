63080ee59ba475c40c4d4cb4c50d4ae68bf28367
Laurent Spagnol <laurent.spagnol@univ-reims.fr>
2024-02-09 22:07:19 +0100

    Ajout indexes

--- schema_sqlite.sql
+++ schema_sqlite.sql
@@ -7,15 +7,22 @@ CREATE TABLE meetings (
 	meeting_end INTEGER NOT NULL,
 	meeting_create INTEGER NOT NULL
 );
+CREATE INDEX idx_meeting_name ON meetings(meeting_name);
+CREATE INDEX idx_meeting_begin ON meetings(meeting_begin);
+CREATE INDEX idx_meeting_end ON meetings(meeting_end);
 
 CREATE TABLE attendees (
 	attendee_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	attendee_meeting_id INTEGER,
 	attendee_meeting_hash TEXT,
 	attendee_role TEXT NOT NULL,
-	attendee_email NOT NULL,
-	attendee_count INTEGER
+	attendee_email TEXT NOT NULL,
+	attendee_count INTEGER DEFAULT 0
 );
+CREATE INDEX idx_attendee_meeting_id ON attendees(attendee_meeting_id);
+CREATE INDEX idx_attendee_meeting_hash ON attendees(attendee_meeting_hash);
+CREATE INDEX idx_attendee_role ON attendees(attendee_role);
+CREATE INDEX idx_attendee_email ON attendees(attendee_email);
 
 CREATE TABLE mail_reminder (
 	mail_reminder_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,6 +30,10 @@ CREATE TABLE mail_reminder (
 	mail_reminder_date INTEGER NOT NULL,
 	mail_reminder_done BOOLEAN
 );
+CREATE INDEX idx_mail_reminder_meeting_id ON mail_reminder(mail_reminder_meeting_id);
+CREATE INDEX idx_mail_reminder_date ON mail_reminder(mail_reminder_date);
+CREATE INDEX idx_mail_reminder_done ON mail_reminder(mail_reminder_done);
+
 
 CREATE TABLE xmpp_reminder (
 	xmpp_reminder_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -30,12 +41,17 @@ CREATE TABLE xmpp_reminder (
 	xmpp_reminder_date INTEGER NOT NULL,
 	xmpp_reminder_done BOOLEAN
 );
+CREATE INDEX idx_xmpp_reminder_meeting_id ON xmpp_reminder(xmpp_reminder_meeting_id);
+CREATE INDEX idx_xmpp_reminder_date ON xmpp_reminder(xmpp_reminder_date);
+CREATE INDEX idx_xmpp_reminder_done ON xmpp_reminder(xmpp_reminder_done);
 
 CREATE TABLE ical (
 	ical_id INTEGER PRIMARY KEY AUTOINCREMENT,
 	ical_owner TEXT NOT NULL,
 	ical_hash TEXT NOT NULL
 );
+CREATE INDEX idx_ical_owner ON ical(ical_owner);
+CREATE INDEX idx_ical_hash ON ical(ical_hash);
 
 CREATE TRIGGER delete_meeting AFTER DELETE ON meetings
 BEGIN
