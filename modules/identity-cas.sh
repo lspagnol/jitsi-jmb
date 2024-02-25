3b0204bf1acc3429400fd1b471043abc6f3890ed
Laurent Spagnol <laurent.spagnol@univ-reims.fr>
2024-02-25 13:38:11 +0100

    Correction -> pas besoin du décodage base64 si le SSO est configuré correctement

--- modules/identity-cas.sh
+++ modules/identity-cas.sh
@@ -10,38 +10,13 @@
 #   pour prendre en compte les attributs CAS
 #   -> directive "CASAuthNHeader CAS_UID" dans la section protégée / CAS
 
-#auth_uid="${HTTP_CAS_UID,,}"
-#auth_name="${HTTP_CAS_GIVENNAME^^} ${HTTP_CAS_SN^^}"
-#auth_mail="${HTTP_CAS_MAIL,,}"
-
-# Les variables retournées par le serveur CAS peuvent être encodées en
+# ATTENTION: vérifier que le SSO ne retourne pas de valeurs encodées en
 # base64 !
 
-if [[ "${HTTP_CAS_UID}" =~ ==$ ]] ; then
-	auth_uid=$(echo -n "${HTTP_CAS_UID}" |base64 -d)
-else
-	auth_uid="${HTTP_CAS_UID}"
-fi
+#auth_uid="${HTTP_CAS_UID,,}"
+auth_uid="${REMOTE_USER,,}"
+auth_mail="${HTTP_CAS_MAIL,,}"
+auth_name="${HTTP_CAS_GIVENNAME^^} ${HTTP_CAS_SN^^}"
 
-if [[ "${HTTP_CAS_MAIL,,}" =~ ==$ ]] ; then
-	auth_mail=$(echo -n "${HTTP_CAS_MAIL}" |base64 -d)
-else
-	auth_mail="${HTTP_CAS_MAIL}"
-fi
-
-if [[ "${HTTP_CAS_GIVENNAME}" =~ ==$ ]] ; then
-	auth_givenname=$(echo -n "${HTTP_CAS_GIVENNAME}" |base64 -d)
-else
-	auth_givenname="${HTTP_CAS_GIVENNAME}"
-fi
-
-if [[ "${HTTP_CAS_SN}" =~ ==$ ]] ; then
-	auth_sn=$(echo -n "${HTTP_CAS_SN}" |base64 -d)
-else
-	auth_sn="${HTTP_CAS_SN}"
-fi
-
-# LOWCASE pour auth_uid et auth_mail, UPPERCASE pour auth_name
-auth_uid="${auth_uid,,}"
-auth_mail="${auth_mail,,}"
-auth_name="${auth_givenname^^} ${auth_sn^^}"
+auth_givenname="{HTTP_CAS_GIVENNAME}"
+auth_sn="${HTTP_CAS_SN}"
